81afd3504a0c56434a9dfac4cd557c113b9ddaec
Dendy Gema <Zenaki Kurniawan>
2016-10-12 11:00:11 +0700

    Tambahan Grafik

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@ int main(int argc, char *argv[])
     qApp->setStyleSheet( "QStatusBar::item { border: 0px}" ) ;
     Login l;
     l.show();
+    l.setGeometry(
+        QStyle::alignedRect(
+            Qt::LeftToRight,
+            Qt::AlignCenter,
+            l.size(),
+            qApp->desktop()->availableGeometry()
+        )
+    );
 
     return a.exec();
 }
